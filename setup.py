66bfc39a5cae75a64520e502ff72740504b475f5
Ahmad Samir <a.samir.atta@gmail.com>
2022-11-12 09:59:06 +0200

    First Commit - Scrapping Notebbok Finished

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Created a tool that estimates data science salaries (MAE ~ $ 11K) to help data scientists negotiate their income when they get a job.',
+    author='Ahmad Samir',
+    license='MIT',
+)
